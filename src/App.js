4f9483fcdc8ca3db3385e1229ddad9af42ac43fe
Prawito Hudoro <prawitohudoro@gmail.com>
2020-04-27 06:36:46 +0700

    install react navigation setup react navigation setup routing / navigasi install stack navigator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Splash, GetStarted} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
 
 const App = () => {
-  return <GetStarted />;
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
 };
 
 export default App;
