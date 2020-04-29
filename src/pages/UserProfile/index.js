d9e53db13a72c6944581215f63e60f9178f54fd8
Prawito Hudoro <prawitohudoro@gmail.com>
2020-04-30 06:29:56 +0700

    add edit profile screen

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Header, List, Profile, Gap} from '../../components';
 
-const UserProfile = () => {
+const UserProfile = ({navigation}) => {
   return (
     <View style={styles.page}>
       <Header title="Profile" />
@@ -14,6 +14,7 @@ const UserProfile = () => {
         desc="Last Update Yesterday"
         type="next"
         icon="edit-profile"
+        onPress={() => navigation.navigate('UpdateProfile')}
       />
       <List
         name="Edit Profile"
