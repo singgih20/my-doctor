1a683a680ad6d300663d7f6653d83d6ab423f3de
Prawito Hudoro <prawitohudoro@gmail.com>
2020-04-30 06:14:22 +0700

    add user profile screen

---
+++ src/pages/UserProfile/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Header, List, Profile, Gap} from '../../components';
+
+const UserProfile = () => {
+  return (
+    <View style={styles.page}>
+      <Header title="Profile" />
+      <Gap height={10} />
+      <Profile />
+      <Gap height={14} />
+      <List
+        name="Edit Profile"
+        desc="Last Update Yesterday"
+        type="next"
+        icon="edit-profile"
+      />
+      <List
+        name="Edit Profile"
+        desc="Last Update Yesterday"
+        type="next"
+        icon="language"
+      />
+      <List
+        name="Edit Profile"
+        desc="Last Update Yesterday"
+        type="next"
+        icon="rate"
+      />
+      <List
+        name="Edit Profile"
+        desc="Last Update Yesterday"
+        type="next"
+        icon="help"
+      />
+    </View>
+  );
+};
+
+export default UserProfile;
+
+const styles = StyleSheet.create({
+  page: {flex: 1, backgroundColor: 'white'},
+});
