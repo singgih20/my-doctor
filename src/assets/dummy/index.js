b506fbef81883fd8cf3f25278de28e1712a9ac0a
Prawito Hudoro <prawitohudoro@gmail.com>
2020-04-30 01:40:56 +0700

    add choose doctor screen

--- src/assets/dummy/index.js
+++ src/assets/dummy/index.js
@@ -5,6 +5,11 @@ import DummyDoctor3 from './doctor3.png';
 import DummyDoctor4 from './doctor4.png';
 import DummyDoctor5 from './doctor5.png';
 import DummyDoctor6 from './doctor6.png';
+import DummyDoctor7 from './doctor7.png';
+import DummyDoctor8 from './doctor8.png';
+import DummyDoctor9 from './doctor9.png';
+import DummyDoctor10 from './doctor10.png';
+import DummyDoctor11 from './doctor11.png';
 import DummyNews1 from './news1.png';
 import DummyNews2 from './news2.png';
 import DummyNews3 from './news3.png';
@@ -20,6 +25,11 @@ export {
   DummyDoctor4,
   DummyDoctor5,
   DummyDoctor6,
+  DummyDoctor7,
+  DummyDoctor8,
+  DummyDoctor9,
+  DummyDoctor10,
+  DummyDoctor11,
   DummyNews1,
   DummyNews2,
   DummyNews3,
