9e15a778081cbc9b0dc865506830f4c0ffb12e51
Prawito Hudoro <prawitohudoro@gmail.com>
2020-05-06 11:41:52 +0700

    add register data to localstorage

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, Input, Loading} from '../../components';
-import {colors, useForm} from '../../utils';
+import {colors, useForm, storeData, getData} from '../../utils';
 import {Fire} from '../../config';
 import {showMessage, hideMessage} from 'react-native-flash-message';
 
@@ -23,7 +23,6 @@ const Register = ({navigation}) => {
       .then(success => {
         setLoading(false);
         setForm('reset');
-        // https://firebase.com/users/i39d9w9chd
         const data = {
           fullName: form.fullName,
           profession: form.profession,
@@ -34,6 +33,8 @@ const Register = ({navigation}) => {
           .ref('users/' + success.user.uid + '/')
           .set(data);
 
+        storeData('user', data);
+        navigation.navigate('UploadPhoto');
         console.log('register success: ', success);
       })
       .catch(error => {
@@ -47,7 +48,6 @@ const Register = ({navigation}) => {
         });
         console.log('error: ', error);
       });
-    // navigation.navigate('UploadPhoto')
   };
   return (
     <>
