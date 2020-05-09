23510af4bc203e3c331822e2fb8e1ee270cdb664
Prawito Hudoro <prawitohudoro@gmail.com>
2020-05-09 08:26:36 +0700

    persiapan halaman chatting sebelum integrasi

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import {Header, ChatItem, InputChat} from '../../components';
 import {fonts, colors} from '../../utils';
 
-const Chatting = ({navigation}) => {
+const Chatting = ({navigation, route}) => {
+  const dataDoctor = route.params;
   return (
     <View style={styles.page}>
       <Header
         type="dark-profile"
-        title="Nairobi Putri Hayza"
+        title={dataDoctor.data.fullName}
+        desc={dataDoctor.data.category}
+        photo={{uri: dataDoctor.data.photo}}
         onPress={() => navigation.goBack()}
       />
       <View style={styles.content}>
-        <Text style={styles.chatDate}>Senin, 21 Maret, 2020</Text>
-        <ChatItem isMe />
-        <ChatItem />
-        <ChatItem isMe />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={styles.chatDate}>Senin, 21 Maret, 2020</Text>
+          <ChatItem isMe />
+          <ChatItem />
+          <ChatItem isMe />
+        </ScrollView>
       </View>
-      <InputChat />
+      <InputChat
+        value=""
+        onChangeText={() => alert('input tap')}
+        onButtonPress={() => alert('buttun press')}
+      />
     </View>
   );
 };
